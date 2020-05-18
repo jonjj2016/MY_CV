f7e416be0665dd9ffdd2f9f61c64c2a4cb2309be
Jon Martirosyan <jonmartirosyan1988@gmail.com>
2020-05-18 02:52:09 -0700

    adding config for gh-pages

---
+++ build/precache-manifest.a4f4069c2de704e05744b971e4285c2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ccce87943688433041caf41c24ae36",
+    "url": "/MY_CV/index.html"
+  },
+  {
+    "revision": "444fa79fb3b091324ae3",
+    "url": "/MY_CV/static/css/2.de514513.chunk.css"
+  },
+  {
+    "revision": "6bf17860bd375b450be1",
+    "url": "/MY_CV/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "444fa79fb3b091324ae3",
+    "url": "/MY_CV/static/js/2.de5c9761.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/MY_CV/static/js/2.de5c9761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf17860bd375b450be1",
+    "url": "/MY_CV/static/js/main.9a0ef284.chunk.js"
+  },
+  {
+    "revision": "e64b10066cc429316b66",
+    "url": "/MY_CV/static/js/runtime-main.8982a2b0.js"
+  },
+  {
+    "revision": "15fcb2db70705824c496e3d383d7ada6",
+    "url": "/MY_CV/static/media/1.15fcb2db.jpg"
+  },
+  {
+    "revision": "dcb55f4402e0d578390abb693189f77a",
+    "url": "/MY_CV/static/media/1.dcb55f44.jpg"
+  },
+  {
+    "revision": "01bfaa84ca4d8f42dd39aa63fabb0b5e",
+    "url": "/MY_CV/static/media/3.01bfaa84.jpg"
+  }
+]);
